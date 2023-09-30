8c4f4d64a68a5a9fe4c33f798110dea95f5d5f26
Parker Mathis <parkerjmathis@gmail.com>
2023-09-30 15:19:38 -0700

    Updated app.js to remove router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Schedule from "./pages/Schedule";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Schedule />}></Route>
-      </Routes>
-    </Router>
-  );
-}
+const App = () => <Schedule />;
 
 export default App;
