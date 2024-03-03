2905fdb0a4b2a35c63e4925e6111a3e96d2d0b85
Parker Mathis <parkermathis@Parkers-MacBook-Pro.local>
2024-03-03 14:03:39 -0800

    Minor bug fix for results cell

--- client/src/components/GameResultCell.js
+++ client/src/components/GameResultCell.js
@@ -4,16 +4,26 @@ import Box from "@mui/material/Box";
 const GameResultCell = ({ game }) => {
   return (
     <>
-      {(game.gameState === "LIVE" || game.gameState === "OFF" || game.gameState === "FINAL") && (
+      {(game.gameState === "LIVE" ||
+        game.gameState === "OFF" ||
+        game.gameState === "FINAL") && (
         <Box>
           {/* LISTS LIVE AND FINAL SCORES */}
-          {game.gameState === "LIVE" && `Live: `} {(game.gameState === "OFF" || game.gameState === "FINAL") && `Final: `}
-          {(game.gameState === "OFF" || game.gameState === "FINAL") && `Final: `}({TeamMap.find((team) => team.id === game.awayTeam.id)?.abbreviation} {game.awayTeam.score},{" "}
-          {TeamMap.find((team) => team.id === game.homeTeam.id)?.abbreviation} {game.homeTeam.score})
+          {
+            TeamMap.find((team) => team.id === game.awayTeam.id)?.abbreviation
+          }{" "}
+          {game.awayTeam.score},{" "}
+          {TeamMap.find((team) => team.id === game.homeTeam.id)?.abbreviation}{" "}
+          {game.homeTeam.score}
+          {game.gameState === "LIVE" && ` [Live]`}{" "}
+          {(game.gameState === "OFF" || game.gameState === "FINAL") &&
+            ` [Final]`}
         </Box>
       )}
       {/* FOR PREVIEW GAMES - DASH ONLY DISPLAYED WHEN RESULT COLUMN HEADER IS PRESENT ON LARGER SCREEN */}
-      <Box sx={{ "@media screen and (max-width: 710px)": { display: "none" } }}>{(game.gameState === "FUT" || game.gameState === "PRE") && <div>-</div>}</Box>
+      <Box sx={{ "@media screen and (max-width: 710px)": { display: "none" } }}>
+        {(game.gameState === "FUT" || game.gameState === "PRE") && <div>-</div>}
+      </Box>
     </>
   );
 };
